e9e1c29d6927e666ef278496f8e62881551bad37
wangpeng <wangpeng022@sina.cn>
2018-02-02 16:59:06 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+// app.get("/", (req, res) => {
+//     res.end("hello")
+// });
+
+app.get("/", (req, res) => {
+    res.send("hello")
+});
+
+
+
+
+
+
+
+
+
+app.use( express.static('public'));
+
+var port = 3000;
+app.listen(port, () => {
+    console.log("http://localhost:"+port);
+})
